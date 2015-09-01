eabb1afc66f710880ba43d6bfcfb9c7be6e97026
Boris Kolpackov <boris@codesynthesis.com>
2015-09-01 13:00:01 +0200

    Initialize build system

---
+++ bpkg/bpkg.cxx
@@ -0,0 +1,13 @@
+// file      : bpkg/bpkg.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2015 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+int
+main ()
+{
+}
---
+++ buildfile
@@ -0,0 +1,7 @@
+# file      : buildfile
+# copyright : Copyright (c) 2014-2015 Code Synthesis Ltd
+# license   : MIT; see accompanying LICENSE file
+
+d = bpkg/
+.: $d
+include $d
