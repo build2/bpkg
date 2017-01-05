7a1192e719b93711014648d7d753c8a64aa366fe
Boris Kolpackov <boris@codesynthesis.com>
2017-01-05 16:01:24 +0200

    Update copyright year

--- bpkg/rep-fetch.cxx
+++ bpkg/rep-fetch.cxx
@@ -1,5 +1,5 @@
 // file      : bpkg/rep-fetch.cxx -*- C++ -*-
-// copyright : Copyright (c) 2014-2016 Code Synthesis Ltd
+// copyright : Copyright (c) 2014-2017 Code Synthesis Ltd
 // license   : MIT; see accompanying LICENSE file
 
 #include <bpkg/rep-fetch>
