e66fbe664f6430c1090c71cf52588070080ff39a
Boris Kolpackov <boris@codesynthesis.com>
2018-07-09 09:52:38 +0200

    Adjust ODB compilation script for bdep-based setup

--- bpkg/odb.sh
+++ bpkg/odb.sh
@@ -3,13 +3,40 @@
 trap 'exit 1' ERR
 
 odb=odb
-lib="\
--I$HOME/work/odb/builds/default/libodb-sqlite-default \
--I$HOME/work/odb/libodb-sqlite \
--I$HOME/work/odb/builds/default/libodb-default \
--I$HOME/work/odb/libodb"
+inc=()
 
-$odb $lib -I.. -I../../libbpkg -I../../libbutl                        \
+if test -d ../.bdep; then
+
+  # Use default configuration for headers.
+  #
+  cfg="$(bdep config list -d .. | \
+sed -r -ne 's#^(@[^ ]+ )?([^ ]+)/ .*default.*$#\2#p')"
+
+  inc+=("-I$(echo "$cfg"/libodb-[1-9]*/)")
+  inc+=("-I$(echo "$cfg"/libodb-sqlite-[1-9]*/)")
+
+  inc+=("-I$cfg/libbutl")
+  inc+=("-I../../libbutl")
+
+  inc+=("-I$cfg/libbpkg")
+  inc+=("-I../../libbpkg")
+
+  inc+=("-I$cfg/bpkg")
+  inc+=("-I..")
+
+else
+
+  inc+=("-I$HOME/work/odb/builds/default/libodb-sqlite-default")
+  inc+=("-I$HOME/work/odb/libodb-sqlite")
+
+  inc+=("-I$HOME/work/odb/builds/default/libodb-default")
+  inc+=("-I$HOME/work/odb/libodb")
+
+  inc+=(-I.. -I../../libbpkg -I../../libbutl)
+
+fi
+
+$odb "${inc[@]}"                                                      \
     -DLIBODB_BUILD2 -DLIBODB_SQLITE_BUILD2 --generate-schema          \
     -d sqlite --std c++14 --generate-query                            \
     --odb-epilogue '#include <bpkg/wrapper-traits.hxx>'               \
