2fc70c76c4971f26ede989da76d4b6dfc7d3ec1d
Karen Arutyunov <karen@codesynthesis.com>
2025-12-01 10:04:30 +0200

    Cleanup skeleton's collect_config() to match config_checksum()

--- bpkg/package-skeleton.cxx
+++ bpkg/package-skeleton.cxx
@@ -2020,8 +2020,8 @@ namespace bpkg
                     dependent_vars_.size () +
                     reflect_.size ()))
     {
-      // For vars we will steal the first non-empty *_vars_. But for sources
-      // reserve the space.
+      // For vars we may steal the first non-empty *_vars_. But for sources
+      // always reserve the space.
       //
       srcs.reserve (n); // At most that many.
 
@@ -2041,8 +2041,8 @@ namespace bpkg
       //
       if (!config_vars_.empty ())
       {
-        // Assign the user source to all the user-specified configuration
-        // variables, with the following exceptions:
+        // Return all the user-specified configuration variables with the
+        // following exceptions:
         //
         // - Skip config.<project>.develop (can potentially be passed by
         //   bdep-init) if the package doesn't use it.
@@ -2054,8 +2054,11 @@ namespace bpkg
         //   currently don't save the expected configuration for system
         //   packages, we will probably need to implement that in the future.
         //
+        // Assign the user source for all the being returned variables.
+        //
         size_t pn (var_prefix_.size ());
-        for (const string& v: config_vars_)
+        bool reserve (true);
+        for (string& v: config_vars_)
         {
           size_t vn;
           if (project_override (v, var_prefix_, &vn))
@@ -2071,7 +2074,14 @@ namespace bpkg
             vn = v.find_first_of ("=+ \t");
           }
 
-          string n (v, 0, vn);
+          if (reserve)
+          {
+            vars.reserve (n);
+            reserve = false;
+          }
+
+          string n (v, 0, vn); // Save the name prior to moving out from v.
+          vars.push_back (move (v));
 
           // Check for a duplicate.
           //
@@ -2084,8 +2094,6 @@ namespace bpkg
           if (i == srcs.end ())
             srcs.push_back (config_variable {move (n), config_source::user});
         }
-
-        vars = move (config_vars_);
       }
 
       // Next dependent configuration.
